5130b76f8cfe5ecdb7ea48aad9ad20ac472e9752
ferreira-joao <joaovfsc231@gmail.com>
2025-08-24 17:11:05 -0300

    progresso aula 22

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
 
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
   return (
-    <div>
-      <Heading attr={123} attr2='string'>
-        Olá mundo!
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quo
-        maxime velit soluta perferendis expedita accusantium officia modi
-        nesciunt officiis fuga sunt distinctio praesentium, accusamus iusto
-        aperiam laborum! Vero, laudantium?
-      </p>
-    </div>
+    <>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
+    </>
   );
 }
