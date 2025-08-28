c5a2f59d43b05659d5d28c3f931397cf2e4fc356
ferreira-joao <joaovfsc231@gmail.com>
2025-08-27 21:38:34 -0300

    progresso aula 24

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 import { Logo } from './components/Logo';
-
 import './styles/theme.css';
 import './styles/global.css';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 export function App() {
   return (
@@ -13,7 +13,11 @@ export function App() {
       </Container>
 
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
